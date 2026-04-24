8e0f1398553a8934d6d9e9fcd29fb2d0f498d944
Ex-Rockstar <ruthirockstar2218@gmail.com>
2026-04-24 09:31:08 +0530

    removed cred

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.string || 'mongodb+srv://admin:2JczkDyrEQAHlrSP@millet-1.yf158fr.mongodb.net/?appName=Millet-1';
+const MONGODB_URI = process.env.string || process.env.MONGODB_URI;
 
 mongoose.connect(MONGODB_URI)
 .then(() => console.log('MongoDB connected!'))
